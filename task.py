8bc1e12b17fd89e69d337e958f227086bbca90c3
Bryce Swarm <bswarm3801@gmail.com>
2017-07-25 17:55:52 -0400

    tasks
    can now write tasks to file

---
+++ task.py
@@ -0,0 +1,72 @@
+import random
+import string
+import csv
+import datetime
+
+class Task:
+    time_format = ('%m/%d/%y')
+
+    def __init__(self, first_name, last_name, task_name,
+                 time_elapsed, notes=None, id=None, **kwargs):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.time_elapsed = time_elapsed
+        self.date = datetime.datetime.today().strftime(self.time_format)
+        self.notes = notes
+        self.task_name = task_name
+
+        if id:
+            self.id = id
+        else:
+            self.id = self.get_id()
+        self.fields = [
+            'id',
+            'first_name',
+            'last_name',
+            'task_name',
+            'time_elapsed',
+            'notes',
+            'date'
+        ]
+
+
+    def __str__(self):
+        str_fmt = ('Date: {}\n'
+                   'Task Name: {}\n'
+                   'First Name: {}\n'
+                   'Last Name: {}\n'
+                   'Time Spent: {}\n'
+                   'Notes: {}')
+        return str_fmt.format(self.date, self.task_name,
+                               self.first_name, self.last_name,
+                               self.time_elapsed, self.notes)
+
+
+    def save_entry(self):
+        with open('work_logs.csv', 'a', newline='') as log_file:
+            writer = csv.DictWriter(log_file, fieldnames=self.fields)
+            writer.writerow({
+                'id' : self.id,
+                'first_name' : self.first_name,
+                'last_name' : self.last_name,
+                'task_name' : self.task_name,
+                'time_elapsed' : self.time_elapsed,
+                'notes' : self.notes,
+                'date' : self.date
+            })
+
+
+    def get_id(self):
+        id_list = []
+        with open('work_logs.csv', 'r') as log_file:
+            reader = csv.DictReader(log_file)
+            for row in reader:
+                id_list.append(row['id'])
+
+        while True:
+            id = ''.join(random.choices(string.ascii_uppercase
+                                        + string.digits, k=10))
+            if id not in id_list:
+                break
+        return id
+
--- work_log.py
+++ work_log.py
@@ -5,19 +5,104 @@
 #
 # Author: Bryce Swarm
 
+import csv
+import os
+
+from task import Task
 
 
+fields = [
+    'id',
+    'first_name',
+    'last_name',
+    'task_name',
+    'time_elapsed',
+    'notes',
+    'date'
+    ]
+
+# clear the command screen
+def clear_screen():
+    """ Clears the command screen for next prompts to be displayed
+        more efficiently."""
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
+def new_task():
+    clear_screen()
+    first_name = input('What is your first name?\n> ')
+    clear_screen()
+    last_name = input('What is your last name?\n> ')
+    clear_screen()
+    task_name = input('What is the task?\n> ')
+    clear_screen()
+    time_elapsed = input('How much time was spent on the task?\n> ')
+    clear_screen()
+    notes = input('Please enter any additional notes.\n> ')
+    task = Task(first_name, last_name, task_name, time_elapsed, notes)
+    task.save_entry()
+    return task
 
 
+def start():
+    try:
+        log_file = open("work_logs.csv", 'x')
+        writer = csv.DictWriter(log_file, fieldnames=fields)
+        writer.writeheader()
+        input('Please enter a new task, press any key to begin.')
+        task = new_task()
+        clear_screen()
+        print('Added Task!\n')
+        print(task)
+        input('\nPress any key to continue.')
+    except FileExistsError:
+        pass
 
+def menu():
+    while True:
+        print('Please make a selection:')
+        print('1: Search\n'
+            '2: Log New Task\n'
+            '3: Quit\n')
+        menu_select = input('> ')
+        if menu_select == '3':
+            clear_screen()
+            quit()
+        elif menu_select == '2':
+            clear_screen()
+            task = new_task()
+            print('Added Task!\n')
+            print(task)
+            input('\nPress any key to continue.')
+        elif menu_select == '1':
+            clear_screen()
+            search_menu()
+        else:
+            clear_screen()
+            print('That is not a valid selection.')
+            continue
 
-
-
+def search_menu():
+    while True:
+        print('How would you like to search?\n'
+              '1: Exact Date\n'
+              '2: Date Range\n'
+              '3: Task Name\n'
+              '4: Return to main menu')
+        menu_select = input('> ')
+        if menu_select == '1':
+            pass
+        if menu_select == '2':
+            pass
+        if menu_select == '3':
+            pass
+        if menu_select == '4':
+            clear_screen()
+            menu()
 
 
 
 
 if __name__ == '__main__':
-    pass
+    start()
+    menu()
